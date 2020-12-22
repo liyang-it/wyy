39c69bc5e452df3e865219661a9fb00ebc391dfa
liyangit <1922802352@qq.com>
2020-12-22 09:54:51 +0800

    提交

--- config/index.js
+++ config/index.js
@@ -5,13 +5,6 @@
 const path = require('path')
 
 module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        data: `@import "~@/assets/scss/variables.scss";`,
-      },
-    }
-  },
     // 关闭 vue-cli3预加载
     chainWebpack: config => {
       // 移除 prefetch 插件
